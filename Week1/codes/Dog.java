7f6593cd741b15efe8d502ed4a4691ff9c7b95af
abm1997 <abmcan@yahoo.com>
2021-01-17 21:40:44 +0200

    still week1

--- Week1/codes/Dog.java
+++ Week1/codes/Dog.java
@@ -1,7 +1,23 @@
 public class Dog {
+    public int weight;
+    
+    /** integer constructor 
+    *now when you make an instance of Dog like Dog d = new Dog(50) 
+    * that's meaning weight of d is 50
+    */
+    
+    public Dog(int w) {
+        w = weight;
+    }
 
-    public static void makeNoise() {
-        System.out.println("Bark!");        
+    public void makeNoise() {
+        if (weight < 30) {
+            System.out.println("yip yipyip");
+        } else if (weight < 60) {
+            System.out.println("bark bark");
+        } else {
+            System.out.println("woooooof");
+        }        
     }
   
 }
--- Week1/codes/DogLauncher.java
+++ Week1/codes/DogLauncher.java
@@ -1,7 +1,14 @@
 public class DogLauncher {
 
     public static void main(String args[]) {
-        Dog.makeNoise();        
+        Dog[] dogs = new Dog[3] ;
+        dog[0] = new Dog(15);
+        dog[1] = new Dog(50);
+        dog[2] = new dog(100);
+
+        dog[0].makeNoise();
+        dog[1].makeNoise();
+        dog[2].makeNoise();
     }   
 }
 
