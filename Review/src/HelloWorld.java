fe35788ac0958bd03505c6459ea34c64e9d94704
abm1997 <abmcan@yahoo.com>
2021-07-08 10:07:55 +0200

    New start after 2 monthes OFF

---
+++ Review/src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static int larger(int x, int y) {
+        if (x>y) {
+            return x;
+        }
+        return y;
+    }
+    public static void main(String[] args) {
+        System.out.println("abdooooooooooooo");
+        int x = 5;
+        System.out.println(x);
+        System.out.println(larger(5,97));
+    }
+}
